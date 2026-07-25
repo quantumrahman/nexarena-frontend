2cb96f646429e6c0efe3c3ef7e1fda724b6f431f
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-07-25 14:53:31 +0600

    refactor: remove copyright text and change layout

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -7,12 +7,9 @@ export const metadata = {
 
 export default function AuthLayout({ children }) {
     return (
-        <main className="relative min-h-screen w-full flex-1 bg-[url('/img/auth.webp')] bg-cover bg-center bg-no-repeat">
-            <div className="absolute inset-0 z-20 bg-linear-to-b from-transparent via-transparent to-[#010717]">
+        <main className="min-h-screen w-full bg-[url('/img/auth.webp')] bg-cover bg-center bg-no-repeat">
+            <div className="relative inset-0 z-20 h-full bg-linear-to-b from-transparent via-transparent to-[#010717]">
                 {children}
-                <p className="fixed bottom-4 left-1/2 z-30 -translate-x-1/2 transform text-sm font-medium text-slate-500">
-                    @ {new Date().getFullYear()} NexArena
-                </p>
             </div>
         </main>
     );
