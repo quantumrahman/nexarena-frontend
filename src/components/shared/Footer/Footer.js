6c43e4c8a3ff86aac89140faaa80e3284dd4e6d9
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-07-25 22:56:05 +0600

    feat: add header and footer components

---
+++ src/components/shared/Footer/Footer.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Footer() {
+    return (
+        <div className="w-full">
+            <h1>footer</h1>
+        </div>
+    );
+}
---
+++ src/components/shared/Header/Header.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Header() {
+    return (
+        <div className="w-full">
+            <h1>Header</h1>
+        </div>
+    );
+}
