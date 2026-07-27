9680399c434186a255097fa0f0ccab558968df6f
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-07-27 20:27:40 +0600

    refactor: implement page heading and facilities components

--- src/modules/FacilitiesModule/FacilitiesModule.js
+++ src/modules/FacilitiesModule/FacilitiesModule.js
@@ -1,9 +1,18 @@
 "use client";
 
+import PageHeading from "@/components/ui/PageHeading/PageHeading";
+import FacilitiesFeature from "@/components/features/FacilitiesFeature/FacilitiesFeature";
+
 export default function FacilitiesModule() {
     return (
-        <div className="w-full">
-            <h1>Facilities module</h1>
-        </div>
+        <>
+            <PageHeading
+                title={"Find Your Perfect Arena"}
+                paragraph={
+                    "Search and book from 200+ premium sports facilities"
+                }
+            />
+            <FacilitiesFeature />
+        </>
     );
 }
