8b13232dc6c8cc4f654f50fb6c0ca68bb01f1b44
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-07-27 15:01:48 +0600

    refactor: fanalize header and footer design

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -1,9 +1,88 @@
 "use client";
 
+import { FaXTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa6";
+
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <div className="w-full">
-            <h1>footer</h1>
-        </div>
+        <footer className="w-full px-5 md:px-6 lg:px-7">
+            <div className="mx-auto w-full max-w-7xl border-y border-slate-900 py-10">
+                <div className="w-full text-center">
+                    <h1 className="text-2xl leading-tight font-bold text-white uppercase">
+                        <span className="text-violet-500">Nex</span>Arena
+                    </h1>
+                </div>
+                <nav className="mt-5 w-full md:mt-10">
+                    <ul className="flex flex-wrap items-center justify-center gap-5">
+                        <li className="text-base font-normal">
+                            <Link
+                                href={"/"}
+                                className="text-slate-500 transition-all duration-200 ease-linear hover:text-slate-400"
+                            >
+                                Privacy policy
+                            </Link>
+                        </li>
+                        <li className="text-base font-normal">
+                            <Link
+                                href={"/"}
+                                className="text-slate-500 transition-all duration-200 ease-linear hover:text-slate-400"
+                            >
+                                Terms & condition
+                            </Link>
+                        </li>
+                        <li className="text-base font-normal">
+                            <Link
+                                href={"/"}
+                                className="text-slate-500 transition-all duration-200 ease-linear hover:text-slate-400"
+                            >
+                                About us
+                            </Link>
+                        </li>
+                        <li className="text-base font-normal">
+                            <Link
+                                href={"/"}
+                                className="text-slate-500 transition-all duration-200 ease-linear hover:text-slate-400"
+                            >
+                                How it works
+                            </Link>
+                        </li>
+                        <li className="text-base font-normal">
+                            <Link
+                                href={"/"}
+                                className="text-slate-500 transition-all duration-200 ease-linear hover:text-slate-400"
+                            >
+                                Support
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="mx-auto flex w-full max-w-7xl flex-wrap items-center justify-between py-4">
+                <span className="text-sm font-medium text-slate-600">
+                    @ {new Date().getFullYear()} NexArena.
+                </span>
+                <div className="flex items-center justify-center gap-3">
+                    <Link
+                        href={"/"}
+                        className="text-sm text-slate-600 transition-all duration-200 ease-linear hover:text-white"
+                    >
+                        <FaLinkedinIn />
+                    </Link>
+                    <Link
+                        href={"/"}
+                        className="text-sm text-slate-600 transition-all duration-200 ease-linear hover:text-white"
+                    >
+                        <FaInstagram />
+                    </Link>
+                    <Link
+                        href={"/"}
+                        className="text-sm text-slate-600 transition-all duration-200 ease-linear hover:text-white"
+                    >
+                        <FaXTwitter />
+                    </Link>
+                </div>
+            </div>
+        </footer>
     );
 }
--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,9 +1,48 @@
 "use client";
 
+import { RiMenu3Fill } from "react-icons/ri";
+
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <div className="w-full">
-            <h1>Header</h1>
-        </div>
+        <header className="fixed top-0 left-0 z-30 w-full px-5 pt-5 md:px-6 md:pt-10 lg:px-7">
+            <div className="mx-auto flex w-full max-w-5xl items-center justify-between rounded-xl border border-slate-900 bg-[#010717]/70 px-4 py-3 backdrop-blur-lg md:px-5 md:py-3">
+                <h1 className="text-lg leading-tight font-bold text-white uppercase">
+                    <span className="text-violet-500">Nex</span>Arena
+                </h1>
+                <nav className="hidden w-fit md:block">
+                    <ul className="flex items-center justify-center gap-4">
+                        <li className="text-base font-medium">
+                            <Link
+                                href={"/"}
+                                className="text-violet-500 transition-colors duration-200 ease-linear hover:text-slate-400"
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li className="text-base font-medium">
+                            <Link
+                                href={"/facilities"}
+                                className="text-slate-500 transition-colors duration-200 ease-linear hover:text-slate-400"
+                            >
+                                Facilities
+                            </Link>
+                        </li>
+                        <li className="text-base font-medium">
+                            <Link
+                                href={"/auth/login"}
+                                className="text-slate-500 transition-colors duration-200 ease-linear hover:text-slate-400"
+                            >
+                                Log in
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <button className="block flex cursor-pointer items-center justify-center text-lg text-white transition-colors duration-200 ease-linear hover:text-violet-500 md:hidden">
+                    <RiMenu3Fill />
+                </button>
+            </div>
+        </header>
     );
 }
