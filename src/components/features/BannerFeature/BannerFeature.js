5c370d0f49c2a1f239e12f223d16a41c58a31742
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-07-27 15:07:34 +0600

    feat: add banner and explore feature components

---
+++ src/components/features/BannerFeature/BannerFeature.js
@@ -0,0 +1,5 @@
+"use client";
+
+export default function BannerFeature() {
+    return <h1>Banner feature</h1>;
+}
---
+++ src/components/features/ExploreFeature/ExploreFeature.js
@@ -0,0 +1,5 @@
+"use client";
+
+export default function ExploreFeature() {
+    return <h1>Exploer feature</h1>;
+}
